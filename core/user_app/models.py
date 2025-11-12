5954046162a7ae88521c0072a43454565dceedc0
Vkluff <100197024+Vkluff@users.noreply.github.com>
2025-11-12 23:57:20 +0100

    Update settings, requirements, and user models; add .env example and gitignore

--- core/user_app/models.py
+++ core/user_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+import uuid
 
 class UserManager(BaseUserManager):
     """Custom user model manager where email is the unique identifier"""
@@ -31,6 +32,24 @@ class User(AbstractUser):
     push_token = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    # Add these fields to avoid clash with auth.User
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
